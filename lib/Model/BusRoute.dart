0e5955cdca8c9aaa6ad37303fbd1b359bad464fb
GregTheo-dev <gregtheo777777@gmail.com>
2025-07-06 18:10:13 +0300

    First commit

--- lib/Model/BusRoute.dart
+++
@@ -1,9 +0,0 @@
-class BusRoute {
-  late final String id;
-  late final String code;
-  late final String line;
-  late final String name;
-  late final String color;
-
-  BusRoute(this.id, this.code, this.line, this.name, this.color);
-}
